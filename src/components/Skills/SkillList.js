5494dc2a065fec59baf80134ea47427e56d26db0
Naveen N Raja <naveennraja@gmail.com>
2026-07-12 21:37:59 +0100

    Add TypeScript to skills list
    Add TypeScript skill with official logo under the JavaScript group

--- src/components/Skills/SkillList.js
+++ src/components/Skills/SkillList.js
@@ -1,56 +1,66 @@
-import html5  from "../../images/skills/html5.png";
-import jsx  from "../../images/skills/JSX.png";
-import css  from "../../images/skills/css3.png";
-import bootstrap  from "../../images/skills/bootstrap.png";
-import reactBootstrap  from "../../images/skills/reactBootstrap.png";
-import sass  from "../../images/skills/sass.png";
-import javascript  from "../../images/skills/javascript.png";
-import jquery  from "../../images/skills/jquery.png";
-import react  from "../../images/skills/react.png";
-import angular  from "../../images/skills/angular.png";
-import nodejs  from "../../images/skills/nodejs.png";
-import amCharts  from "../../images/skills/amCharts.png";
-import greensock  from "../../images/skills/greensock.png";
-import gatsby  from "../../images/skills/gatsby.png";
-import mongodb from "../../images/skills/mongodb.png";
-import visualStudio  from "../../images/skills/visualStudio.png";
-import Webstorm  from "../../images/skills/Webstorm.png";
-import BBEdit  from "../../images/skills/BBEdit.png";
+import html5 from "../../images/skills/html5.png"
+import jsx from "../../images/skills/JSX.png"
+import css from "../../images/skills/css3.png"
+import bootstrap from "../../images/skills/bootstrap.png"
+import reactBootstrap from "../../images/skills/reactBootstrap.png"
+import sass from "../../images/skills/sass.png"
+import javascript from "../../images/skills/javascript.png"
+import typescript from "../../images/skills/typescript.png"
+import jquery from "../../images/skills/jquery.png"
+import react from "../../images/skills/react.png"
+import angular from "../../images/skills/angular.png"
+import nodejs from "../../images/skills/nodejs.png"
+import amCharts from "../../images/skills/amCharts.png"
+import greensock from "../../images/skills/greensock.png"
+import gatsby from "../../images/skills/gatsby.png"
+import mongodb from "../../images/skills/mongodb.png"
+import visualStudio from "../../images/skills/visualStudio.png"
+import Webstorm from "../../images/skills/Webstorm.png"
+import BBEdit from "../../images/skills/BBEdit.png"
 
 const commonImage = {
-     "html" : {
-          "HTML" : html5,
-          "JSX" : jsx
+     html: {
+          HTML: html5,
+          JSX: jsx,
      },
-     "css" : {
-          "CSS" : css,
-          "Bootstrap" : bootstrap,
-          "ReactBootstrap" : reactBootstrap,
-          "Sass" : sass
+     css: {
+          CSS: css,
+          Bootstrap: bootstrap,
+          Sass: sass,
      },
-     "javascript" : {
-          "Javascript" : javascript,
-          "jQuery" : jquery,
-          "React" :react,
-          "Angular" : angular,
-          "Node" :nodejs,
-          "amCharts" :amCharts,
-          "Greensock" : greensock,
-          "Gatsby" : gatsby,
-          "MongoDB" : mongodb
+     javascript: {
+          JavaScript: javascript,
+          TypeScript: typescript,
+          React: react,
+          Angular: angular,
+          Node: nodejs,
+          amCharts: amCharts,
+          Gatsby: gatsby,
+          MongoDB: mongodb,
      },
-     "tools" :{
-          "Visual Studio" : visualStudio,
-          "Webstorm" : Webstorm,
-          "BBedit": BBEdit
-     }
+     tools: {
+          "Visual Studio": visualStudio,
+          Webstorm: Webstorm,
+     },
 }
 
 // Flat map of skill name → image src for use in ProjectsList
 export const skills = {
-     html5, jsx, css, bootstrap, reactBootstrap, sass,
-     javascript, jquery, react, angular, nodejs, amCharts, greensock, gatsby, mongodb,
-     visualStudio, Webstorm, BBEdit,
+     html5,
+     jsx,
+     css,
+     bootstrap,
+     sass,
+     javascript,
+     typescript,
+     react,
+     angular,
+     nodejs,
+     amCharts,
+     gatsby,
+     mongodb,
+     visualStudio,
+     Webstorm,
 }
 
 export default commonImage
